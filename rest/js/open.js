2d0eff80928aa4bb168c20386bf037a2796b65c0
MVTSS <matisse.allaux@hotmail.com>
2024-02-14 20:51:45 +0100

    Get back to nothing, end of teo project
    No need to put it here for any longer

--- rest/js/open.js
+++
@@ -1,8 +0,0 @@
-const BTN = document.getElementById("open");
-BTN.addEventListener("click", jsp); // Remove parentheses from jsp function call
-
-const PTITTXT = document.getElementById("ptittexte");
-
-function jsp() {
-    PTITTXT.classList.toggle("invisible"); // Uncomment this line
-}
